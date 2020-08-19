b8ab4833be0008ded8278829019c6f66815a4b2c
Adrian Räuchle <a.raeuchle@gmx.de>
2020-08-19 22:45:33 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numberOfRounds) => {
+    setGuessRounds(numberOfRounds);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen rounds={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />;
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    flex: 1
+  }
 });
---
+++ screens/GameScreen.js
@@ -0,0 +1,77 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rand = Math.floor(Math.random() * (max - min)) + min; 
+
+    if (rand === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rand;
+    }
+};
+
+const GameScreen = props => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+    const [rounds, setRounds] = useState(0);
+
+    const {userChoice, onGameOver } = props;
+
+    // Runs after rendering component
+    useEffect(() => {
+            if (currentGuess === userChoice) {
+                onGameOver(rounds)
+            }
+    }, [currentGuess, userChoice, onGameOver]);
+
+    const nextGuessHandler = (direction) => {
+        if ((direction === 'lower' && currentGuess < props.userChoice) ||  (direction === 'greater' && currentGuess > props.userChoice)) {
+            Alert.alert('Dont cheat!', 'You know that this is wrong...', [{text: 'Sorry!', style: 'cancel'}]);
+            return;
+        }
+
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's guess: </Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
+            </Card>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+});
+
+export default GameScreen;
